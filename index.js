551c85fd44bdad214b8b9f9d1531d797477ceaf6
ceesiyamoah <ccyamoah@gmail.com>
2020-09-21 02:32:19 +0000

    add equal button function

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const display = document.querySelector('.row.display')
 const cells = document.querySelectorAll('.row.numbers>div.cell.number, .cell.zero')
 const operators = document.querySelectorAll('div.cell.operator:not(.equal)')
 const clear = document.querySelector('.cell.clear')
-console.log(clear)
+const backspace = document.querySelector('.cell.backspace')
+const equal = document.querySelector('.cell.operator.equal')
 
 
-//function to clear the display
+//*function to clear the display
 const emptyDisplay = () =>
 {
     display.innerHTML = ''
 }
 
 
-//Function to display 0 
+//*Function to display 0 
 const displayZero = () =>
 {
     display.innerHTML = '0'
 }
 
 
+//*Backspace implementation
+backspace.addEventListener('click', () =>
+{
+    display.innerHTML = display.innerHTML.slice(0, display.innerHTML.length - 1)
+    if (display.innerHTML.length === 0) {
+        displayZero()
+    }
+})
+
+//TODO: finish up
+equal.addEventListener('click', () =>
+{
+
+})
 
 
-//making numbers interactive
+//*making numbers interactive
 cells.forEach(cell =>
 {
     cell.addEventListener('click', () =>
@@ -35,7 +50,7 @@ cells.forEach(cell =>
 });
 
 
-//making clear button interactive
+//*making clear button interactive
 clear.addEventListener('click', () =>
 {
     emptyDisplay()
@@ -43,13 +58,12 @@ clear.addEventListener('click', () =>
 })
 
 
-//making operators (= not included) interactive
+//*making operators (= not included) interactive
 operators.forEach(operator =>
 {
     operator.addEventListener('click', () =>
     {
-        operandLeft = parseInt(display.innerHTML)
-        console.log(operandLeft)
+        //TODO implement algorithm
         displayZero()
     }
     )
